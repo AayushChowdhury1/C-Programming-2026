31d51f2da1e7cad7d2f53cc6c536a55a4353a853
NotColtPlayZ <116013622+AayushChowdhury1@users.noreply.github.com>
2026-07-06 08:11:11 +0530

    To Convert Temperature from Celsius to Fahrenheit.

---
+++ BASIC/C_TO_F.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float c,f;
+	printf("Enter Value in Celsius:");
+	scanf("%f",&c);
+	f=((c*9)/5)+32;
+	printf("Value in Fahrenheit is:%.2f",f);
+	getch();
+}
