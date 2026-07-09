aa3963e6514560adfbccbcb49b289fb84a8f8336
NotColtPlayZ <116013622+AayushChowdhury1@users.noreply.github.com>
2026-07-09 16:10:52 +0530

    To Find Position of A Number

---
+++ Relational Operators/POSOFNO.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a,n;
+	clrscr();
+	printf ("Enter a number:");
+	scanf ("%d",&a);
+	if (a>=1){
+	printf ("n=1\n");}
+	else if (n=0){
+	printf ("n=0\n");}
+	else
+	printf ("n=-1\n");
+	getch();
+}
