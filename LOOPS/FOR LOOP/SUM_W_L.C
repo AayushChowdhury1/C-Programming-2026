fcb58af726a688fc8df2456897fa421f1b5ea6e7
NotColtPlayZ <116013622+AayushChowdhury1@users.noreply.github.com>
2026-07-16 23:49:04 +0530

    To Find the sum of digits of a number

---
+++ LOOPS/FOR LOOP/SUM_W_L.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int n,s=0;
+	clrscr();
+	printf("Enter a Number:");
+	scanf("%d",&n);
+	while(n>0
+	{
+		s=s+n%10;
+		n=n/10;
+	}
+	printf("Sum of digits=%d",s);
+	getch();
+)
